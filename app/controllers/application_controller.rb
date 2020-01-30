c00b57fec8b7193d3326df4e473210a74a263908
aibo <hokuto100retsuken@pepabo.com>
2020-01-30 12:18:39 +0900

    クエリ情報からロケールを変更する

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
     before_action :detect_locale
 
+    # 現在のロケール設定でlocaleオプションをデフォルト設定
+    def detect_url_options(options = {})
+        { locale: I18n.locale }
+    end
+
     private
         def detect_locale
-            I18n.locale = request.headers['Accept-Language'].scan(/\A[a-z]{2}/).first
+            # ブラウザの設定からロケールを取得する
+            # I18n.locale = request.headers['Accept-Language'].scan(/\A[a-z]{2}/).first
+            # クエリ情報からロケールを変更する
+            I18n.locale = params[:locale]
         end
 end
