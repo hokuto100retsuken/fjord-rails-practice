7e0a3c5e2e6ba9fcab7da320cf6d52ec5637c37c
aibo <hokuto100retsuken@pepabo.com>
2020-01-30 10:59:31 +0900

    ブラウザの言語設定に応じてロケールを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    before_action :detect_locale
+
+    private
+        def detect_locale
+            I18n.locale = request.headers['Accept-Language'].scan(/\A[a-z]{2}/).first
+        end
 end
