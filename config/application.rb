9eb7bc2beb12a72a7c1c1e898e18c1b943ff7f93
aibo <hokuto100retsuken@pepabo.com>
2020-02-05 11:38:36 +0900

    Revert "rubocopを通してコードのフォーマットを整えた"
    This reverts commit b20204c2b0e44367b8c197a0b0c4f0fbe794a924.

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
+require_relative 'boot'
 
-require_relative "boot"
-
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -13,7 +11,7 @@ module BooksApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
 
     config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
