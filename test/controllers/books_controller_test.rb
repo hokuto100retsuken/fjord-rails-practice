d9af7a42b0984aff3d5b1629100f24ad5b3beab3
aibo <hokuto100retsuken@pepabo.com>
2020-02-05 15:07:03 +0900

    Revert "rubocopによるコードフォーマッティングを適用した"
    This reverts commit b6a1d40627d42c48b78787c92d595a843bebc6a3.

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -1,6 +1,4 @@
-# frozen_string_literal: true
-
-require "test_helper"
+require 'test_helper'
 
 class BooksControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -18,7 +16,7 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create book" do
-    assert_difference("Book.count") do
+    assert_difference('Book.count') do
       post books_url, params: { book: { memo: @book.memo, title: @book.title } }
     end
 
@@ -41,7 +39,7 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should destroy book" do
-    assert_difference("Book.count", -1) do
+    assert_difference('Book.count', -1) do
       delete book_url(@book)
     end
 
